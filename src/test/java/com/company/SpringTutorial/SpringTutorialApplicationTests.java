666288ff3473033cebf8277a555898d2b46a5ad0
Ignacio Gomez <ignacio.gomez@solarisbank.de>
2021-04-16 14:34:00 +0200

    test GH actions with broken test

--- src/test/java/com/company/SpringTutorial/SpringTutorialApplicationTests.java
+++ src/test/java/com/company/SpringTutorial/SpringTutorialApplicationTests.java
@@ -1,7 +1,11 @@
 package com.company.SpringTutorial;
 
+import com.company.SpringTutorial.model.Person;
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.UUID;
 
 @SpringBootTest
 class SpringTutorialApplicationTests {
@@ -10,4 +14,17 @@ class SpringTutorialApplicationTests {
 	void contextLoads() {
 	}
 
+
+	@Test
+	public void TestShouldTestSomething() {
+		Person marcus = new Person(UUID.randomUUID(), "marcus");
+		Assert.assertEquals("marcus", marcus.getName());
+
+	}
+
+	@Test
+	public void TestShouldBreakCI() {
+		Person marcus = new Person(UUID.randomUUID(), "marcus");
+		Assert.assertEquals("daniel", marcus.getName());
+	}
 }
